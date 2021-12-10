8a78ff3beb2f85cfb222de0fe78bde5ce5dd643b
Ross, Andy <andsross@iu.edu>
2021-12-10 14:38:42 -0500

    AT Push

--- javascript.js
+++ javascript.js
@@ -30,13 +30,14 @@ document.getElementById("C1").innerHTML = data[cArray[0]].NAME,
 document.getElementById("C2").innerHTML = data[cArray[1]].NAME
 ))
 }
+
 function ATRosterData() {
   const gArray = Array(4) // array size is 10
   				.fill()
   				.map(() => Math.floor(106 * Math.random()));
   const sfArray = Array(2) // array size is 10
   				.fill()
-  				.map(() => Math.floor(Math.random() * (107-74 + 1) + 107));
+  				.map(() => Math.floor(Math.random() * (107-74 + 1) + 74));
   const fcArray = Array(2) // array size is 10
           .fill()
           .map(() => Math.floor(Math.random() * (168-108 + 1) + 108));
@@ -50,15 +51,26 @@ console.log(gArray,sfArray,fcArray,cArray);
   })
   .then(data => (console.log(data),
 //Array for g, array for F, array for C
-document.getElementById("PG1").innerHTML = data[gArray[0]].NAME,
-document.getElementById("PG2").innerHTML = data[gArray[1]].NAME,
-document.getElementById("SG1").innerHTML = data[gArray[2]].NAME,
-document.getElementById("SG2").innerHTML = data[gArray[3]].NAME,
-document.getElementById("SF1").innerHTML = data[sfArray[0]].NAME,
-document.getElementById("SF2").innerHTML = data[sfArray[1]].NAME,
-document.getElementById("PF1").innerHTML = data[fcArray[0]].NAME,
-document.getElementById("PF2").innerHTML = data[fcArray[1]].NAME,
-document.getElementById("C1").innerHTML = data[cArray[0]].NAME,
-document.getElementById("C2").innerHTML = data[cArray[1]].NAME
-))
+document.getElementById("PG1").innerHTML = data[gArray[0]].Player,
+document.getElementById("PG2").innerHTML = data[gArray[1]].Player,
+document.getElementById("SG1").innerHTML = data[gArray[2]].Player,
+document.getElementById("SG2").innerHTML = data[gArray[3]].Player,
+document.getElementById("SF1").innerHTML = data[sfArray[0]].Player,
+document.getElementById("SF2").innerHTML = data[sfArray[1]].Player,
+document.getElementById("PF1").innerHTML = data[fcArray[0]].Player,
+document.getElementById("PF2").innerHTML = data[fcArray[1]].Player,
+document.getElementById("C1").innerHTML = data[cArray[0]].Player,
+document.getElementById("C2").innerHTML = data[cArray[1]].Player
+));
+
+// if ((data[gArray[0]] || data[gArray[1]]) == (data[gArray[2]] || data[gArray[3]])) {
+//   ATRosterData();
+// },
+// else if (data[sfArray[0]] == data[sfArray[1]]) {
+//   ATRosterData();
+// },
+// else if ((data[pfArray[0]] || data[pfArray[1]]) == (data[cArray[0]] || data[cArray[1]])) {
+//   ATRosterData();
+// }
+
 }
