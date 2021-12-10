5c995dd868eb6d5d4dc5416daa4b258fa49640f7
Ross, Andy <andsross@iu.edu>
2021-12-10 13:37:58 -0500

    All-Time data and index function

--- javascript.js
+++ javascript.js
@@ -1,25 +1,64 @@
 function RosterData() {
-  // var mydata = JSON.parse(data);
-  // console.log(mydata);
-  const array = Array(10) // array size is 10
+  const gArray = Array(4) // array size is 10
   				.fill()
-  				.map(() => Math.floor(192 * Math.random()));
-
-  console.log(array);
-  fetch("https://aro2s.github.io/RosterGen/data.json")
+  				.map(() => Math.floor(96 * Math.random()));
+  const sfArray = Array(2) // array size is 10
+  				.fill()
+  				.map(() => Math.floor(Math.random() * (135-97 + 1) + 97));
+  const fcArray = Array(2) // array size is 10
+          .fill()
+          .map(() => Math.floor(Math.random() * (198-135 + 1) + 135));
+  const cArray = Array(2) // array size is 10
+          .fill()
+          .map(() => Math.floor(Math.random() * (198-169 + 1) + 169));
+console.log(gArray,sfArray,fcArray,cArray);
+  fetch("https://aro2s.github.io/RosterGen/tempData.json")
   .then(response => {
      return response.json();
   })
   .then(data => (console.log(data),
-document.getElementById("PG1").innerHTML = data[array[0]].NAME,
-document.getElementById("PG2").innerHTML = data[array[1]].NAME,
-document.getElementById("SG1").innerHTML = data[array[2]].NAME,
-document.getElementById("SG2").innerHTML = data[array[3]].NAME,
-document.getElementById("SF1").innerHTML = data[array[4]].NAME,
-document.getElementById("SF2").innerHTML = data[array[5]].NAME,
-document.getElementById("PF1").innerHTML = data[array[6]].NAME,
-document.getElementById("PF2").innerHTML = data[array[7]].NAME,
-document.getElementById("C1").innerHTML = data[array[8]].NAME,
-document.getElementById("C2").innerHTML = data[array[9]].NAME
+//Array for g, array for F, array for C
+document.getElementById("PG1").innerHTML = data[gArray[0]].NAME,
+document.getElementById("PG2").innerHTML = data[gArray[1]].NAME,
+document.getElementById("SG1").innerHTML = data[gArray[2]].NAME,
+document.getElementById("SG2").innerHTML = data[gArray[3]].NAME,
+document.getElementById("SF1").innerHTML = data[sfArray[0]].NAME,
+document.getElementById("SF2").innerHTML = data[sfArray[1]].NAME,
+document.getElementById("PF1").innerHTML = data[fcArray[0]].NAME,
+document.getElementById("PF2").innerHTML = data[fcArray[1]].NAME,
+document.getElementById("C1").innerHTML = data[cArray[0]].NAME,
+document.getElementById("C2").innerHTML = data[cArray[1]].NAME
+))
+}
+function ATRosterData() {
+  const gArray = Array(4) // array size is 10
+  				.fill()
+  				.map(() => Math.floor(106 * Math.random()));
+  const sfArray = Array(2) // array size is 10
+  				.fill()
+  				.map(() => Math.floor(Math.random() * (107-74 + 1) + 107));
+  const fcArray = Array(2) // array size is 10
+          .fill()
+          .map(() => Math.floor(Math.random() * (168-108 + 1) + 108));
+  const cArray = Array(2) // array size is 10
+          .fill()
+          .map(() => Math.floor(Math.random() * (168-133 + 1) + 133));
+console.log(gArray,sfArray,fcArray,cArray);
+  fetch("https://aro2s.github.io/RosterGen/ATtempData.json")
+  .then(response => {
+     return response.json();
+  })
+  .then(data => (console.log(data),
+//Array for g, array for F, array for C
+document.getElementById("PG1").innerHTML = data[gArray[0]].NAME,
+document.getElementById("PG2").innerHTML = data[gArray[1]].NAME,
+document.getElementById("SG1").innerHTML = data[gArray[2]].NAME,
+document.getElementById("SG2").innerHTML = data[gArray[3]].NAME,
+document.getElementById("SF1").innerHTML = data[sfArray[0]].NAME,
+document.getElementById("SF2").innerHTML = data[sfArray[1]].NAME,
+document.getElementById("PF1").innerHTML = data[fcArray[0]].NAME,
+document.getElementById("PF2").innerHTML = data[fcArray[1]].NAME,
+document.getElementById("C1").innerHTML = data[cArray[0]].NAME,
+document.getElementById("C2").innerHTML = data[cArray[1]].NAME
 ))
 }
